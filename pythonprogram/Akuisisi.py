d1b59d75e2cff3dec3e1d081e356187baa5989dc
Yusuf Anggara <93311604+Anggara101@users.noreply.github.com>
2021-11-16 13:48:16 +0700

    Update Akuisisi.py

--- pythonprogram/Akuisisi.py
+++ pythonprogram/Akuisisi.py
@@ -12,7 +12,9 @@ abd_den = preprocesing.pp(abd)
 datalogging.savecsv(x, abd_den, 'data1.csv')
 # peak and amplitude detection
 abd_amp, abd_peak, ns = preprocesing.peakvalley(abd_den)
+
 # Clustering
-Clustering.Case1(abd_amp, abd_peak, x, abd_den, ns)
+abd_peak1i, abd_peak2i = Clustering.Case4(abd_amp, abd_peak, x, abd_den, ns)
+datalogging.plotresult(x, abd_den, abd_peak1i, abd_peak2i)
 
 plt.show()
