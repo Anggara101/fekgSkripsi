848929ebb2ce692a0d7a468fd5d1f819b458aa84
Yusuf Anggara <93311604+Anggara101@users.noreply.github.com>
2021-11-18 14:26:51 +0700

    Update preprocesing.py

--- pythonprogram/preprocesing.py
+++ pythonprogram/preprocesing.py
@@ -12,35 +12,35 @@ def pp(abd):
     for i in range(1, 9):  # Remove detail coefficient
         coeffs[i] = np.zeros_like(coeffs[i])
     a = pywt.waverec(coeffs, 'sym4')
-    abd_noBW = abd - a
+    abd_nobw = abd - a
     # remove noise
-    abd_den = denoise_wavelet(abd_noBW, wavelet='sym4', mode='hard', wavelet_levels=4, rescale_sigma='True')
+    abd_den = denoise_wavelet(abd_nobw, wavelet='sym4', mode='hard', wavelet_levels=4, rescale_sigma='True')
     return abd_den
 
 
 def peakvalley(abd_den):
     # detect peak and valley
-    abd_peakI, _ = sci.find_peaks(abd_den)
-    abd_valleyI, _ = sci.find_peaks(-abd_den)
+    abd_peaki, _ = sci.find_peaks(abd_den)
+    abd_valleyi, _ = sci.find_peaks(-abd_den)
     # plt.plot(abd_den)
     # plt.plot(abd_peakI, abd_den[abd_peakI], "r*")
     # plt.plot(abd_valleyI, abd_den[abd_valleyI], "y*")
 
     # amplitude of peak followed by the next valley
     # if the first signal is valley, delete the first valley
-    if abd_peakI[0] > abd_valleyI[0]:
-        for i in range(len(abd_valleyI) - 1):
-            abd_valleyI[i] = abd_valleyI[i + 1]
+    if abd_peaki[0] > abd_valleyi[0]:
+        for i in range(len(abd_valleyi) - 1):
+            abd_valleyi[i] = abd_valleyi[i + 1]
     # check length of peak and valley
-    if len(abd_peakI) > len(abd_valleyI):
-        abd_peakI = np.delete(abd_peakI, -1)
-    if len(abd_valleyI) > len(abd_peakI):
-        abd_valleyI = np.delete(abd_valleyI, -1)
-    abd_peak = abd_den[abd_peakI]
-    abd_valley = abd_den[abd_valleyI]
+    if len(abd_peaki) > len(abd_valleyi):
+        abd_peaki = np.delete(abd_peaki, -1)
+    if len(abd_valleyi) > len(abd_peaki):
+        abd_valleyi = np.delete(abd_valleyi, -1)
+    abd_peak = abd_den[abd_peaki]
+    abd_valley = abd_den[abd_valleyi]
     # Amplitude
     abd_amp = abd_peak - abd_valley
-    ns = abd_valleyI - abd_peakI
+    ns = abd_valleyi - abd_peaki
     # plt.figure()
     # plt.plot(abd_amp,"*")
     return abd_amp, abd_peak, ns
